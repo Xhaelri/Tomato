d04b7c9996e28b64abd4fddeae732a0d7e254577
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2025-06-10 05:29:40 +0300

    update

--- food-delivery-app/src/App.jsx
+++ food-delivery-app/src/App.jsx
@@ -1,35 +1,34 @@
-import React, { useState } from 'react'
-import Home from './pages/Home/Home'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import MyOrders from './pages/MyOrders/MyOrders'
-import StoreContextProvider from './Context/StoreContext'
+import React, { useState } from "react";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import MyOrders from "./pages/MyOrders/MyOrders";
+import StoreContextProvider from "./context/StoreContext";
 
 const App = () => {
-
-  const [showLogin,setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    <StoreContextProvider>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/order' element={<PlaceOrder/>}/>
-          <Route path='/myorder' element={<MyOrders/>}/>
-        </Routes>
-      </div>
-      <Footer />
+      <StoreContextProvider>
+        {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+        <div className="app">
+          <Navbar setShowLogin={setShowLogin} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+            <Route path="/myorder" element={<MyOrders />} />
+          </Routes>
+        </div>
+        <Footer />
       </StoreContextProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
