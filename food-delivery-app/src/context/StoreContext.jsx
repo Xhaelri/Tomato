861c754ea276d16983d9cd9958ac45564b2894d6
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2025-06-10 05:26:53 +0300

    update

--- food-delivery-app/src/context/StoreContext.jsx
+++ food-delivery-app/src/context/StoreContext.jsx
@@ -1,11 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list,menu_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const [ordersData,setOrdersData] = useState({});
     
     const addToCart = (itemId) =>{
         if(!cartItems[itemId])
